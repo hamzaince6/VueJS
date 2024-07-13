7b6aa06e53763c0be074dda84f1760f64da674f7
Hamza İnce <hamzaince001@gmail.com>
2024-07-13 10:46:59 +0300

    vue js router contact added

---
+++ 08-Vue-Router/router-app/src/views/Contact.vue
@@ -0,0 +1,26 @@
+<template>
+  <section>
+    <h1>Contact</h1>
+    <div v-for="contact in contacts" :key="contact.id">
+      <p>{{ contact.email }}</p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      contacts: [
+        { id: 1, name: "John", email: "john@example.com" },
+        { id: 2, name: "Ely", email: "ely@example.com" },
+        { id: 3, name: "Carl", email: "carl@example.com" }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
